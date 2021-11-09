2eba9021659e95ee6d8044a959b7d8389a457242
Drakatoa <54646819+Drakatoa@users.noreply.github.com>
2021-11-08 18:37:55 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: "check ping",
     execute(message, args, cmd, client, Discord) {
-        message.channel.send(`Pong!🏓 Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        message.channel.send(`🏓 Pong! Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 }
