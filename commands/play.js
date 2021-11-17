f7ea6025861bdc4b56934ddb82c07255c429cd07
Drakatoa <54646819+Drakatoa@users.noreply.github.com>
2021-11-16 22:25:56 -0600

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -5,7 +5,7 @@ const queue = new Map();
 
 module.exports = {
     name: 'play',
-    aliases: ['p','skip', 'stop'], 
+    aliases: ['skip', 'stop'], 
     cooldown: 0,
     description: 'MUSIC POGGIES',
     async execute(message, args, cmd, client, Discord){
