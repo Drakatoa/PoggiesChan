98faf65648c9d95cd4b25021511e1f016f708224
Drakatoa <54646819+Drakatoa@users.noreply.github.com>
2021-12-28 23:12:16 -0600

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'ping',
+    description: "check ping",
+    execute(message, args, cmd, client, Discord) {
+        message.channel.send(`Pong!🏓 Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    }
+}  
